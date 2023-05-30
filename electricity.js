23e5aa9f1e62d6d91f58b3556ff88268715effd1
Avile Sindelo <avilesindelo@gmail.com>
2023-05-30 11:26:21 +0200

    electricity app factory function first commit

--- electricity.js
+++ electricity.js
@@ -1,5 +1,11 @@
 function Electricity() {
 
+    let state = {
+        unitsAvailable: 0,
+        amount: 0,
+        advance: false
+    };
+
     // do we want to go with this or array? 
     let appliances = {
         'Stove': 10, 
@@ -9,12 +15,21 @@ function Electricity() {
     };
 
     function topUpElectricity(amount) {
-
+        if(amount == 10){
+            state.unitsAvailable += 7;
+        } else if(amount == 20){
+            state.unitsAvailable += 14;
+        } else if(amount == 50){
+            state.unitsAvailable += 35;
+        } else {
+            //advance
+            state.unitsAvailable += 21;
+        }
 
     }
 
     function getUnitsAvailable() {
-         return unitsAvailable;
+        return state.unitsAvailable;
     }
 
     /*
@@ -22,7 +37,7 @@ function Electricity() {
     * other wise return false and do nothing.
     */
     function useAppliance(appliance) {
-        
+        if(state.unitsAvailable >= appliances)
     }
 
     function advanceTaken() {
@@ -41,6 +56,5 @@ function Electricity() {
         useAppliance,
         totalAmountSpent,
         totalUnitsBought
-
     }
 }
