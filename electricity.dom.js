e0369deb17dca66b56058fe5eddb1f935f01b529
Avile Sindelo <avilesindelo@gmail.com>
2023-05-30 12:42:44 +0200

    defining the useApplication function

--- electricity.dom.js
+++ electricity.dom.js
@@ -4,6 +4,12 @@
 
 // Factory Function instance 
 const electricity =  Electricity();
-
-
+electricity.topUpElectricity(10);
+electricity.topUpElectricity(50);
+console.log('units available before use :', electricity.getUnitsAvailable());
+electricity.useAppliance('Stove');
+electricity.useAppliance('Kettle');
+electricity.useAppliance('TV');
+electricity.useAppliance('Fridge');
+console.log('units available after use :', electricity.getUnitsAvailable());
 // DOM events here 
--- electricity.js
+++ electricity.js
@@ -3,6 +3,7 @@ function Electricity() {
     let state = {
         unitsAvailable: 0,
         amount: 0,
+        unitsBought: 0,
         advance: false
     };
 
@@ -17,15 +18,20 @@ function Electricity() {
     function topUpElectricity(amount) {
         if(amount == 10){
             state.unitsAvailable += 7;
+            state.unitsBought += 7;
         } else if(amount == 20){
             state.unitsAvailable += 14;
+            state.unitsBought += 14;
         } else if(amount == 50){
             state.unitsAvailable += 35;
+            state.unitsBought += 35;
         } else {
             //advance
             state.unitsAvailable += 21;
+            state.unitsBought += 21;
         }
-
+        //add the amount spent in the amount property of the state variable as it denotes the total amount spent
+        state.amount += amount;
     }
 
     function getUnitsAvailable() {
@@ -37,16 +43,23 @@ function Electricity() {
     * other wise return false and do nothing.
     */
     function useAppliance(appliance) {
-        if(state.unitsAvailable >= appliances)
+        if(appliances[appliance] <= state.unitsAvailable){
+            state.unitsAvailable -= appliances[appliance];
+            return true;
+        } else {
+            return false;
+        }
     }
 
     function advanceTaken() {
     }
 
     function totalAmountSpent() {
+        return state.amount;
     }
 
     function totalUnitsBought(){
+        return state.unitsBought;
     }
 
     return {
@@ -58,3 +71,4 @@ function Electricity() {
         totalUnitsBought
     }
 }
+
